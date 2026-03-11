00287c99017f1eccf39d893b2723811187d2335b
ES_PREPA_DEV <sosoros66@icloud.com>
2026-03-11 11:16:35 +0100

    init test projet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleRPG.App.Utils;
+
+namespace ConsoleRPG;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var test = ColorFormat.GetColoredString("Hello World!", Color256.White, Color256.DarkGreen);
+        Console.WriteLine(test);
+    }
+}
