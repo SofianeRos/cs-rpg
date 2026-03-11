f6151a768460bdbf895f1774feddda71f8fd8b35
ES_PREPA_DEV <sosoros66@icloud.com>
2026-03-11 11:46:15 +0100

    cahracter cs add

--- App/Gameplay/PlayableCharacter/Character.cs
+++ App/Gameplay/PlayableCharacter/Character.cs
@@ -2,5 +2,16 @@ namespace ConsoleRPG.App.Gameplay.PlayableCharacter;
 
 public abstract class Character
 {
+    protected string name;
+    protected int agility;
+    protected int armor;
+    protected int hp;
+    protected int speed;
+    protected int strength;
+    protected int stamina;
     
+    public Character(string name)
+    {
+        this.name = name;
+    }
 }
